87bd48b6f97164db4d01d09f775653f2e1d1bb08
willyflies1 <whfiles7@gmail.com>
2024-08-16 10:14:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Library API
+
+## An api built for accessing and managing a book collection.
---
+++ library-api/src/test/java/com/whfiles/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whfiles.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
